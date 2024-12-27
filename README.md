9b71e3dd91f411cb8dc6c64d96c69a5329c663f9
seanEidrefNatividad <seaneidrefnatividad@gmail.com>
2024-12-27 14:30:58 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## Challenge done! -Sean Eidref Natividad
+
 # File Metadata Microservice
 
 This is the boilerplate for the File Metadata Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice
--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 var express = require('express');
 var cors = require('cors');
+const bodyParser = require('body-parser');
+const multer  = require('multer');
+const upload = multer();
+
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req,res)=>{
+  const {originalname, mimetype, size} = req.file;
+  res.json({"name":originalname,"type":mimetype,size})
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
